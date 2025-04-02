ee51cfb86071a116444fffc7a920191bf9c8e6ad
harrizon <lucasharrizon@gmail.com>
2025-04-02 10:37:44 +0300

    added items page

--- app/src/main/java/com/harry/sokomart/ui/screens/item/ItemScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/item/ItemScreen.kt
@@ -2,17 +2,31 @@ package com.harry.sokomart.ui.screens.item
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import java.lang.reflect.Modifier
+import com.harry.sokomart.ui.theme.mytheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ItemScreen(navController: NavController){
     Column (
         modifier = Modifier.fillMaxSize()
-    ){  }
+    ){
+        //TopAppBar
+        TopAppBar(
+            title = { Text(text = "Item") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = mytheme
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
