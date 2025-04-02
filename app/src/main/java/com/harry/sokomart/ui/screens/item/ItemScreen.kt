425ba3bb482eb783c8829c7cdde6852ea3523371
harrizon <lucasharrizon@gmail.com>
2025-04-02 10:28:01 +0300

    empty commit

--- app/src/main/java/com/harry/sokomart/ui/screens/item/ItemScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/item/ItemScreen.kt
@@ -1,14 +1,18 @@
 package com.harry.sokomart.ui.screens.item
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
 
 @Composable
 fun ItemScreen(navController: NavController){
-
-
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ){  }
 }
 
 @Preview(showBackground = true)
