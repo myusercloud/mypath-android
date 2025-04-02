fc5a2dd1cda784d0552c15493c2924a91fb80b98
harrizon <lucasharrizon@gmail.com>
2025-04-02 10:53:06 +0300

    items menu

--- app/src/main/java/com/harry/sokomart/ui/screens/item/ItemScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/item/ItemScreen.kt
@@ -3,7 +3,11 @@ package com.harry.sokomart.ui.screens.item
 import android.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -26,8 +30,14 @@ fun ItemScreen(navController: NavController){
             title = { Text(text = "Item") },
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = mytheme,
-                titleContentColor = white
-            )
+                titleContentColor = white,
+                navigationIconContentColor = white
+            ),
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                }
+            }
         )
     }
 }
