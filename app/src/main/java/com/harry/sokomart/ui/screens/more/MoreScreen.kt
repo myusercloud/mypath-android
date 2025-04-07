31bd2b6a02cf616e477e8297b36133c8e6a9fda4
harrizon <lucasharrizon@gmail.com>
2025-04-07 09:47:42 +0300

    added products screen

--- app/src/main/java/com/harry/sokomart/ui/screens/more/MoreScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/more/MoreScreen.kt
@@ -1,7 +1,9 @@
 package com.harry.sokomart.ui.screens.more
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
@@ -15,6 +17,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.harry.sokomart.ui.theme.mytheme
@@ -52,6 +55,7 @@ fun MoreScreen(navController: NavController){
 
             )
         //End of TopAppBar
+        Spacer(modifier = Modifier.height(20.dp))
     }
 
 }
