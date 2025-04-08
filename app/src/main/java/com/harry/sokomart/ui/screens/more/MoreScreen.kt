0362390300ec42a2fd8bd992ce51d8ea6fb51ea7
harrizon <lucasharrizon@gmail.com>
2025-04-08 09:04:45 +0300

    started tue mrng commits

--- app/src/main/java/com/harry/sokomart/ui/screens/more/MoreScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/more/MoreScreen.kt
@@ -1,14 +1,19 @@
 package com.harry.sokomart.ui.screens.more
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
@@ -98,7 +103,30 @@ fun MoreScreen(navController: NavController){
         )
         Spacer(modifier = Modifier.height(20.dp))
         //row start
-
+        Row (
+            modifier = Modifier.padding(start = 20.dp).horizontalScroll(rememberScrollState())
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "home",
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Image(
+                painter = painterResource(id = R.drawable.img_6),
+                contentDescription = "home",
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Image(
+                painter = painterResource(id = R.drawable.img_7),
+                contentDescription = "home",
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Image(
+                painter = painterResource(id = R.drawable.img_8),
+                contentDescription = "home",
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
         //row end
     }
 
