35c4d2c621caed849a148f34e024725be1cb37c5
harrizon <lucasharrizon@gmail.com>
2025-04-10 10:59:26 +0300

    added a background image in the home screen

--- app/src/main/java/com/harry/sokomart/ui/screens/service/ServiceScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/service/ServiceScreen.kt
@@ -36,108 +36,10 @@ import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ServiceScreen(navController: NavController){
-
-
-    //Scaffold
-
-    var selectedIndex by remember { mutableStateOf(0) }
-
-    Scaffold(
-        //TopBar
-        topBar = {
-            TopAppBar(
-                title = { Text("Item Screen") },
-                navigationIcon = {
-                    IconButton(onClick = { /* Handle back/nav */ }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.LightGray,
-                    titleContentColor = Color.White,
-                    navigationIconContentColor = Color.White
-                )
-            )
-        },
-
-        //BottomBar
-        bottomBar = {
-            NavigationBar(
-                containerColor = Color.LightGray
-            ){
-                NavigationBarItem(
-                    icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-                    label = { Text("Home") },
-                    selected = selectedIndex == 0,
-                    onClick = { selectedIndex = 0
-                        //navController.navigate(ROUT_HOME)
-                    }
-                )
-                NavigationBarItem(
-                    icon = { Icon(Icons.Default.Favorite, contentDescription = "Favorites") },
-                    label = { Text("Favorites") },
-                    selected = selectedIndex == 1,
-                    onClick = { selectedIndex = 1
-                        // navController.navigate(ROUT_HOME)
-                    }
-                )
-                NavigationBarItem(
-                    icon = { Icon(Icons.Default.Person, contentDescription = "Profile") },
-                    label = { Text("Profile") },
-                    selected = selectedIndex == 2,
-                    onClick = { selectedIndex = 2
-                        //  navController.navigate(ROUT_HOME)
-                    }
-                )
-
-            }
-        },
-
-        //FloatingActionButton
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /* Add action */ },
-                containerColor = Color.LightGray
-            ) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        },
-        content = { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .fillMaxSize()
-            ) {
-
-
-                //Main Contents of the page
-                Text(text = "Welcome to Homescreen Screen", fontSize = 20.sp)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text("This is where the main content goes.")
-
-
-
-
-
-
-
-
-
-
-
-
-            }
-        }
-    )
-
-    //End of scaffold
-
+fun ServiceScreen(navController: NavController) {
 
 
 }
-
-
 @Preview(showBackground = true)
 @Composable
 fun ServiceScreenPreview(){
