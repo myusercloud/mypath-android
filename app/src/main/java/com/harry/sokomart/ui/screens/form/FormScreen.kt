b301435cf3ecf35fad87d21df93e3e17e2a772ba
harrizon <lucasharrizon@gmail.com>
2025-04-11 10:24:23 +0300

    created a form

--- app/src/main/java/com/harry/sokomart/ui/screens/form/FormScreen.kt
+++ app/src/main/java/com/harry/sokomart/ui/screens/form/FormScreen.kt
@@ -1,6 +1,7 @@
 package com.harry.sokomart.ui.screens.form
 
 import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -29,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,6 +39,7 @@ import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.harry.sokomart.R
 import com.harry.sokomart.ui.theme.blue
 import com.harry.sokomart.ui.theme.mytheme
 
@@ -46,6 +50,11 @@ fun FormScreen(navController: NavController){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "home",
+            modifier = Modifier.size(250.dp),
+        )
         Text(
             text = "Sign Up",
             fontSize = 50.sp,
